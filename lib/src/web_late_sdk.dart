ea9b88c6edfcac7f90d4447b2be1dd997f0024d6
alexandr.fedorenko <alexandr.fedorenko@onix-systems.com>
2023-02-16 10:57:58 +0200

    feat(update_6): Added support of fallback local translations file

--- lib/src/web_late_sdk.dart
+++ lib/src/web_late_sdk.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:weblate_sdk/src/const.dart';
 import 'package:weblate_sdk/src/storage/impl/hive_storage_impl.dart';
 import 'package:weblate_sdk/src/storage/impl/preferences_storage_impl.dart';
 import 'package:weblate_sdk/src/util/custom_types.dart';
 import 'package:weblate_sdk/src/web_late_localization_delegate.dart';
 import 'package:weblate_sdk/src/client/weblate_client.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class WebLateSdk {
   WebLateSdk._();
@@ -13,6 +18,7 @@ class WebLateSdk {
   static bool _isInitialized = false;
   static TranslationsMap _translations = {};
   static late String _defaultLanguage;
+  static LanguageKeys? _fallbackTranslations;
 
   /// Initialize library. Call this function in [main] function of your app;
   /// [accessKey] - you can find it in your WebLate profile in Api Access section;
@@ -27,6 +33,8 @@ class WebLateSdk {
   /// [disableCache] - disable or enable caching. By default cache
   /// disabled on debug and enabled on release;
   /// [cacheLive] - cache live time. By default 2 hours;
+  /// [fallbackJson] - set local JSON translations file for case when you
+  /// do not have internet connection and cached translations;
   static Future<bool> initialize({
     required String accessKey,
     required String host,
@@ -35,11 +43,18 @@ class WebLateSdk {
     required String defaultLanguage,
     bool? disableCache,
     Duration? cacheLive,
+    String? fallbackJson,
   }) async {
     if (_isInitialized) {
       return _isInitialized;
     }
     _defaultLanguage = defaultLanguage;
+    if (fallbackJson != null) {
+      _fallbackTranslations = await _initializeFallbackJson(
+        fallbackJson,
+      );
+    }
+
     final storage = HiveStorageImpl();
     await storage.initialize();
     final preferences = PreferencesStorageImpl();
@@ -61,6 +76,26 @@ class WebLateSdk {
     return _isInitialized;
   }
 
+  static Future<LanguageKeys> _initializeFallbackJson(
+    String fallbackJson,
+  ) async {
+    try {
+      final fallbackFileContent = await rootBundle.loadString(fallbackJson);
+      final jsonData =
+          await json.decode(fallbackFileContent) as Map<String, dynamic>;
+      return jsonData.map(
+        (key, value) => MapEntry(key, value as String),
+      );
+    } catch (e, _) {
+      if (kDebugMode) {
+        print(
+          '${Const.storageIOError}: Could fallback not read file $fallbackJson. Fallback translations will not be used.\n$e',
+        );
+      }
+    }
+    return {};
+  }
+
   /// Check is library can be initialized;
   /// On first app run app requires internet connection to cache translations strings;
   Future<bool> canInitialize() => _client.canInitialize();
@@ -75,9 +110,18 @@ class WebLateSdk {
   static TranslationsMap get translations => _translations;
 
   /// Is initialization completed or not;
-  static bool get isInitializedSuccessfully =>
-      _isInitialized && translations.isNotEmpty;
+  static bool get isFullyInitialized =>
+      isSDKInitialized && isTranslationsFetchSuccess;
+
+  /// Is initialization completed or not;
+  static bool get isSDKInitialized => _isInitialized;
+
+  /// Is translations fetched successfully or not;
+  static bool get isTranslationsFetchSuccess => translations.isNotEmpty;
 
   /// Get default language. By default means completed;
   static String get defaultLanguage => _defaultLanguage;
+
+  /// Get local fallback translations;
+  static LanguageKeys? get fallbackTranslations => _fallbackTranslations;
 }
